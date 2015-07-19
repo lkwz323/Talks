3cdf989254528513f85be20247eed1082e3aed2f
lkwzlkwz <lkwz323@163.com>
2015-07-19 14:14:58 +0800

    web

---
+++ code/Talks.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Talks.Web.MvcApplication" Language="C#" %>
---
+++ code/Talks.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Talks.Web.Startup))]
+namespace Talks.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
