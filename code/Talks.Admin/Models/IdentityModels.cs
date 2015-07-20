01afb094cbcd7f502a13e4576e000ebcd7c52ddb
lzw <lkwz@qq.com>
2015-07-20 22:00:32 +0800

    fix

--- code/Talks.Admin/Models/IdentityModels.cs
+++ code/Talks.Admin/Models/IdentityModels.cs
@@ -3,10 +3,13 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
 
 namespace Talks.Admin.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+   
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
@@ -29,5 +32,6 @@ namespace Talks.Admin.Models
         {
             return new ApplicationDbContext();
         }
+
     }
 }
