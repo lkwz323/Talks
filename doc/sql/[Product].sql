b04883b4240075b920892c5d3877eedbfcd0ab7f
li.zw <li.zw@yougou.com>
2015-07-23 15:01:10 +0800

    db table

--- doc/sql/[Product].sql
+++ doc/sql/[Product].sql
@@ -1,7 +1,67 @@
 USE [TalksDB]
 GO
 
-/****** Object:  Table [dbo].[Product]    Script Date: 07/22/2015 16:16:37 ******/
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__Product__Product__060DEAE8]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF__Product__Product__060DEAE8]
+END
+
+GO
+
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__Product__Name__07020F21]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF__Product__Name__07020F21]
+END
+
+GO
+
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__Product__Descrip__07F6335A]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF__Product__Descrip__07F6335A]
+END
+
+GO
+
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__Product__Company__08EA5793]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF__Product__Company__08EA5793]
+END
+
+GO
+
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__Product__Price__09DE7BCC]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF__Product__Price__09DE7BCC]
+END
+
+GO
+
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF__Product__Categor__0AD2A005]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF__Product__Categor__0AD2A005]
+END
+
+GO
+
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF_Product_AddTime]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Product] DROP CONSTRAINT [DF_Product_AddTime]
+END
+
+GO
+
+USE [TalksDB]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 07/23/2015 15:00:38 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Product]') AND type in (N'U'))
+DROP TABLE [dbo].[Product]
+GO
+
+USE [TalksDB]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 07/23/2015 15:00:38 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -18,6 +78,7 @@ CREATE TABLE [dbo].[Product](
 	[Company] [varchar](500) NOT NULL,
 	[Price] [decimal](18, 2) NOT NULL,
 	[Category] [varchar](50) NOT NULL,
+	[AddTime] [datetime] NOT NULL,
  CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
 (
 	[ProductID] ASC
@@ -45,6 +106,9 @@ ALTER TABLE [dbo].[Product] ADD  DEFAULT ((0)) FOR [Price]
 GO
 
 ALTER TABLE [dbo].[Product] ADD  DEFAULT ('') FOR [Category]
+GO
+
+ALTER TABLE [dbo].[Product] ADD  CONSTRAINT [DF_Product_AddTime]  DEFAULT (getdate()) FOR [AddTime]
 GO
 
 
